47ebbd4cd8f05056c9872e5b34e34dccf16bcbcf
seongbae <pseongbae@gmail.com>
2025-08-20 17:32:18 +0900

    Feat: Apply pagination

--- backend/locations/views.py
+++ backend/locations/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Location
 from .serializers import LocationSerializer
 
 
-class LocationList(generics.ListAPIView):
-    queryset = Location.objects.all()
-    serializer_class = LocationSerializer
+class LocationList(APIView):
+    def get(self, request):
+        try:
+            page = int(request.query_params.get("page", 1))
+        except ValueError:
+            page = 1
+        count_size = 10
+        start = (page - 1) * count_size
+        end = start + count_size
+
+        all_locations = Location.objects.all()
+        serializer = LocationSerializer(
+            all_locations[start:end],
+            many=True,
+        )
+        return Response(serializer.data)
